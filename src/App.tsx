c4df025a8b7629a225bce31a166f7b0a30e02871
Alexis Aoun <alexis.aoun@hotmail.com>
2025-03-08 17:36:55 +0100

    base grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function SquaredGrid({n}: {n: number}) {
+  const elements = Array.from({length: n}, (_, index) => (
+    <div className='box'>{index}</div>
+  ));
+  return <div className='grid'>{elements}</div>
+}
+
 function App() {
+  const size: number = 28*28;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <SquaredGrid n={size}></SquaredGrid>
+        <p className='App-text'>
+          reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
